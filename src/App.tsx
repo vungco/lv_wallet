1f3047b78e76e95e0c162191126054ca7e2cdbeb
vungco <vung2k3@gmail.com>
2025-08-18 16:36:53 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 const App = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold text-blue-500">
-        Hello React + TS + Tailwind + Webpack!
-      </h1>
+      <h1 className="text-green-500">Hello React + TS + Tailwind + Webpack!</h1>
     </div>
   );
 };
